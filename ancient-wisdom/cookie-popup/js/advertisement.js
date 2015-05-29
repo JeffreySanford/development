55ae0f7909f3249cdfd5469571eeb37b895d5b79
Jeffrey Sanford <jeffreysanford@gmail.com>
2015-05-29 02:18:40 -0400

    cookies page for ancient wisdom update

--- ancient-wisdom/cookie-popup/js/advertisement.js
+++ ancient-wisdom/cookie-popup/js/advertisement.js
@@ -1,7 +1,10 @@
 var firstWords = ['Who', 'do', 'you', 'think', 'pays', 'for', 'this', 'site', '?????'];
 var answers = ['Santa Claus', 'John Galt', 'The Free Masons', 'The Illuminati', 'Gold sales from Nibiru'];
 var secondWords = ['Please', 'consider donating', 'today', 'http:clickable.donate.link', 'Thanks', 'You will now be directed to the website'];
-var targetContent = document.getElementById("targetContent");
+var targetContentFirst = document.getElementById("targetContentFirst");
+var targetContentAnswers = document.getElementById("targetContentAnswers");
+var targetContentSecond = document.getElementById("targetContentSecond");
+
 
 function sleep(milliseconds) {
   var wait = 0;
@@ -16,18 +19,39 @@ function sleep(milliseconds) {
 function showFirstWords() {
   var i = 0;
   for (i; i < firstWords.length; i++) {
-    sleep(100);
-    targetContent.innerHTML = firstWords[i];
+    sleep(1000);
+    targetContentFirst.innerHTML += " " + firstWords[i];
     console.log('Item ' + i + ' is currently set to ' + firstWords[i]);
+  }
+}
+
+function showAnswers() {
+  var i = 0;
+  for (i; i < answers.length; i++) {
+    sleep(500);
+    targetContentAnswers.innerHTML = " " + answers[i];
+    console.log('Item ' + i + ' is currently set to ' + answers[i]);
+  }
+}
+
+function showSecondWords() {
+  var i = 0;
+  for (i; i < secondWords.length; i++) {
+    sleep(250);
+    targetContentSecond.innerHTML = " " + secondWords[i];
+    console.log('Item ' + i + ' is currently set to ' + secondWords[i]);
   }
 }
 
 function display() {
   // x functionality when window loads
-  targetContent.innerHTML = "This has been set";
+  targetContentFirst.innerHTML = "";
   showFirstWords();
   console.log("do something after first");
   sleep(5000);
+  showAnswers();
+  sleep(5000);
+  showSecondWords();
 }
 
 document.getElementById("continueButton").onclick = function () {
@@ -35,6 +59,8 @@ document.getElementById("continueButton").onclick = function () {
   console.log("buton clicked");
 };
 
+/*
 window.onload = function () {
-  display();
+  display;
 };
+ */
