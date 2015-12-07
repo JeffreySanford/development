c3e289af076862344b3af77f205207167428d681
Jeffrey Sanford <Jeffrey Sanford>
2015-12-06 17:45:29 -0800

    working on D3JS models linechart and scatter chart added

--- idea-construction/angular-sandbox/server.js
+++
@@ -1,9 +0,0 @@
-(function IFEE() {
-    'use strict';
-
-    var connect = require('connect');
-    var serveStatic = require('serve-static');
-
-    console.log("development server has been started on port 8080");
-    connect().use(serveStatic(__dirname)).listen(8080);
-}());
